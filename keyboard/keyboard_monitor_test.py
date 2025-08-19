163206bc91c5d5594154a58a3db32a07af264917
yDewolf <andrersng@gmail.com>
2025-08-19 20:16:54 -0300

    Fix some bugs with upper letters

--- keyboard/keyboard_monitor_test.py
+++ keyboard/keyboard_monitor_test.py
@@ -1,15 +1,23 @@
-from pynput.keyboard import Key, Listener
+from pynput.keyboard import Key, Listener, KeyCode
 import os
 import time
 from datetime import datetime
 
+from pynput.keyboard._base import KeyCode
+
 FILE_NAME = "test_save"
 
-class KeyboardHeatmap:
+
+class KeyboardHeatmap :
     total_key_presses = 0
+    key_pressed = {}
+
+    last_moment_key_pressed = {}
+
+    longest_key_press_interval = {}
+    smallest_key_press_interval = {}
 
     key_press_count = {}
-    keys_start_hold_time = {}
     keys_hold_time = {}
 
     keys_max_hold_time = {}
@@ -17,21 +25,39 @@ class KeyboardHeatmap:
     consecutive_end = 0
 
     def start_recording(self):
-        with Listener(on_press=self.on_press, on_release=self.on_release) as listener: # type: ignore
-            listener.join()
+            with Listener(on_press=self.on_key_press, on_release=self.on_key_release) as listener: # type: ignore
+                listener.join()
+            
+            self.save_to_file()
 
-        self.save_to_file(FILE_NAME)
+    def on_key_press(self, key: Key | KeyCode):
+        key_name = str(key).lower()
+        
+        if self.key_pressed.get(key_name, False) == True:
+            return
+        
+        if self.key_press_count.get(key_name, -1) == -1:
+            self.key_press_count[key_name] = 0
 
-    def on_press(self, key):
-        print(f"Pressed {key}")
-        if self.key_press_count.get(key, -1) == -1:
-            self.key_press_count[key] = 0
+            self.keys_max_hold_time[key_name] = 0
+            self.keys_hold_time[key_name] = 0
+            self.last_moment_key_pressed[key_name] = time.time()
+        
+        print(f"Pressed {key_name}")
 
-            self.keys_max_hold_time[key] = 0
-            self.keys_hold_time[key] = 0
-            self.keys_start_hold_time[key] = time.time()
+        self.key_pressed[key_name] = True
 
-        self.key_press_count[key] += 1
+        interval = time.time() - self.last_moment_key_pressed[key_name]
+        self.last_moment_key_pressed[key_name] = time.time()
+        
+        if self.key_press_count[key_name] == 0:
+            self.smallest_key_press_interval[key_name] = 2 ** 16
+            self.longest_key_press_interval[key_name] = interval
+        else:
+            self.longest_key_press_interval[key_name] = max(self.longest_key_press_interval[key_name], interval)
+            self.smallest_key_press_interval[key_name] = min(self.smallest_key_press_interval[key_name], interval)
+
+        self.key_press_count[key_name] += 1
         self.total_key_presses += 1
 
         if key == Key.end:
@@ -42,25 +68,44 @@ class KeyboardHeatmap:
         if self.consecutive_end == 3:
             return False
     
-    def on_release(self, key):
-        self.keys_hold_time[key] += time.time() - self.keys_start_hold_time[key]
-        self.keys_max_hold_time[key] = max(self.keys_hold_time[key], self.keys_max_hold_time[key])
+    def on_key_release(self, key: Key | KeyCode):
+        key_name = str(key).lower()
+        
+        if self.key_press_count.get(key_name, None) == None:
+            print(f"Skipped key: {key_name}")
+            return
 
-    def save_to_file(self, file_name):
+        print(f"released {key_name}")
+        self.key_pressed[key_name] = False
+
+        current_hold_time = time.time() - self.last_moment_key_pressed[key_name]
+
+        self.keys_hold_time[key_name] += current_hold_time
+        self.keys_max_hold_time[key_name] = max(current_hold_time, self.keys_max_hold_time[key_name])
+
+    def save_to_file(self):
         with open(os.path.join(os.path.dirname(__file__), "saves", ((str) (datetime.strftime(datetime.now(),"%d-%m-%Y_%H-%M-%S")) + ".hmp")), "w+") as file:
-            stringified = f"v0.3\n[GENERAL_INFO]\ntotal_presses{self.total_key_presses}\n"
+            stringified = f"v0.4\n[GENERAL_INFO]\ntotal_presses: {self.total_key_presses}\n"
 
             stringified += "[KEY_PRESSES]\n"
             for key in self.key_press_count:
-                stringified += f"{key}: {self.key_press_count[key]}\n"
+                stringified += f"{key}: {(self.key_press_count[key])}\n"
+
+            stringified += "[LONGEST_KEY_INTERVAL]\n"
+            for key in self.longest_key_press_interval:
+                stringified += f"{key}: {int(round(self.longest_key_press_interval[key] * 1000))}\n"
+
+            stringified += "[SMALLEST_KEY_INTERVAL]\n"
+            for key in self.smallest_key_press_interval:
+                stringified += f"{key}: {int(round(self.smallest_key_press_interval[key] * 1000))}\n"
 
             stringified += "[KEY_HOLD_TIMES]\n"
             for key in self.keys_hold_time:
-                stringified += f"{key}: {self.keys_hold_time[key]}\n"
+                stringified += f"{key}: {int(round(self.keys_hold_time[key] * 1000))}\n"
             
             stringified += "[MAX_KEY_HOLD_TIMES]\n"
             for key in self.keys_max_hold_time:
-                stringified += f"{key}: {self.keys_max_hold_time[key]}\n"
+                stringified += f"{key}: {int(round(self.keys_max_hold_time[key] * 1000))}\n"
             
             file.write(stringified)
 
