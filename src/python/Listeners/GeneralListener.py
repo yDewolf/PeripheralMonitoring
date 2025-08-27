413a5b98268d2ec5e8a5760dcc8203fef640236a
yDewolf <andrersng@gmail.com>
2025-08-27 16:19:58 -0300

    Parse events function (WIP)

--- src/python/Listeners/GeneralListener.py
+++ src/python/Listeners/GeneralListener.py
@@ -5,9 +5,18 @@ from pynput import mouse
 import threading
 import atexit
 import time
+from enum import Enum
+
+class EventTypes(Enum):
+    MOUSE_MOVE = 0
+    MOUSE_CLICK = 1
+    KEYBOARD_PRESS = 2
+    KEYBOARD_RELEASE = 3 
+
 
 class GeneralListener:
     keyboard_listener: KeyboardListener
+    paused: bool = False
     running: bool = True
 
     mouse_thread: threading.Thread
@@ -24,7 +33,7 @@ class GeneralListener:
         self.keyboard_thread.start()
 
         try:
-            while True:
+            while self.running:
                 # Change this to a update rate
                 time.sleep(0.0016)
         except KeyboardInterrupt:
@@ -35,21 +44,72 @@ class GeneralListener:
     def stop(self):
         self.running = False
         print("Quitting properly")
+
         self.mouse_thread.join()
         self.keyboard_thread.join()
 
+
     def listen_keyboard(self):
         while self.running:
-            with keyboard.Events() as events:
-                event = events.get(1)
-                print(f"Keyboard event: {event}")
+            if self.paused:
+                continue
+            
+            thread_paused: bool = False
+            try:
+                with keyboard.Events() as events:
+                    event = events.get(1)
+                    if event is None:
+                        continue
+
+                    event_type: EventTypes
+
+                    if type(event) is keyboard.Events.Press:
+                        event_type = EventTypes.KEYBOARD_PRESS
+                    if type(event) is keyboard.Events.Release:
+                        event_type = EventTypes.KEYBOARD_RELEASE
+
+                    self.parse_event(event_type, event)
+            except Exception:
+                self.paused = True
+                thread_paused = True
+            
+            finally:
+                if thread_paused:
+                    self.paused = False
+                # print(f"Keyboard event: {event}")
         
         print("Stopped keyboard listener")
 
     def listen_mouse(self):
         while self.running:
-            with mouse.Events() as events:
-                event = events.get(1)
-                print(f"Mouse event: {event}")                    
-        
+            if self.paused:
+                continue
+            
+            thread_paused: bool = False
+            try:
+                with mouse.Events() as events:
+                    event = events.get(1)
+                    if event is None:
+                        continue
+
+                    event_type: EventTypes
+                    if type(event) is mouse.Events.Click:
+                        event_type = EventTypes.MOUSE_CLICK
+                    if type(event) is mouse.Events.Move:
+                        event_type = EventTypes.MOUSE_MOVE
+
+                    self.parse_event(event_type, event)
+                    # print(f"Mouse event: {event}")                    
+            except Exception:
+                self.paused = True
+                thread_paused = True
+            
+            finally:
+                if thread_paused:
+                    self.paused = False
+
         print("Stopped mouse listener")
+    
+
+    def parse_event(self, type: EventTypes, event):
+        print(f"Received Event! | type: {type.name} | event: {event}")
